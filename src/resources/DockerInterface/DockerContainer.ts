d4663c4f0e489ea011d9bd6310e252b4cf4c12f3
tiagonapoli <napoli.tiago96@gmail.com>
2019-07-05 14:55:11 -0300

    Improve DockerInterface

--- src/resources/DockerInterface/DockerContainer.ts
+++ src/resources/DockerInterface/DockerContainer.ts
@@ -1,80 +1,73 @@
+import { Subprocess, SubprocessIO } from './../../utils/Subprocess'
 import { HostPortProvider } from './HostPortProvider'
 import { Logger } from '../../utils/Logger'
-import { Subprocess } from './Subprocess'
 
-class DockerContainer {
+interface DockerContainerOptions {
   imageName: string
-  dockerProc: any
   gpuCapable: boolean
   detach: boolean
-  port: string
+  port: number
   network: string
   envVariables: string[]
   dnsName: string
+}
 
-  constructor({ imageName, gpuCapable, port, detach, network, envVariables, dnsName }: any) {
-    this.imageName = imageName
-    this.port = port
-    this.gpuCapable = gpuCapable
-    this.detach = detach
-    this.network = network
-    this.envVariables = envVariables
-    this.dnsName = dnsName
-  }
+export class DockerContainer {
+  docker: Subprocess
+  id: string
+  opts: DockerContainerOptions
 
-  public run() {
-    this.dockerProc = new Subprocess({
+  public start: (io: SubprocessIO) => void
+  public getExitCode: () => void
+  public getErr: () => void
+  public getOut: () => void
+  public getExitSignal: () => void
+  public finish: () => void
+  public getError: () => void
+
+  constructor(id: string, options: DockerContainerOptions) {
+    this.opts = options
+    this.id = id
+
+    this.docker = new Subprocess({
+      id,
       path: process.env.DOCKER_BINARY_PATH || '/usr/bin/docker',
       args: this.createRunArgs(),
     })
 
-    return this.dockerProc.start()
-  }
-
-  public getExitCode() {
-    return this.dockerProc.exitCode()
-  }
-
-  public getStdErr() {
-    return this.dockerProc.getStdErr()
+    this.start = this.docker.start
+    this.getExitCode = this.docker.getExitCode
+    this.getErr = this.docker.getErr
+    this.getOut = this.docker.getOut
+    this.getExitSignal = this.docker.getExitSignal
+    this.finish = this.docker.finish
+    this.getError = this.docker.getError
   }
 
   private createRunArgs() {
+    const { imageName, gpuCapable, detach, port, network, envVariables, dnsName } = this.opts
+
     let args = ['run']
-    this.gpuCapable && args.push('--runtime=nvidia')
-    if (this.port) {
-      args.push(`--expose=${this.port}`)
-      args = args.concat([`-e`, `PORT=${this.port}`])
-    }
+    if (gpuCapable) args.push('--runtime=nvidia')
+    if (port != null) args = args.concat([`-e`, `PORT=${port}`])
+    if (dnsName != null) args.push(`--name=${dnsName}`)
+    if (network != null) args.push(`--network=${network}`)
+    if (detach) args.push('-d')
 
-    if (this.dnsName) {
-      args.push(`--name=${this.dnsName}`)
-    }
-
-    if (this.network) {
-      args.push(`--network=${this.network}`)
-    }
-
-    if (this.envVariables) {
-      this.envVariables.forEach(envVar => {
+    if (envVariables != null) {
+      envVariables.forEach(envVar => {
         args = args.concat(['-e', envVar])
       })
     }
 
-    if (this.detach) {
-      args.push('-d')
-    }
-
     if (process.env.NODE_ENV === 'development') {
       const hostPort = HostPortProvider.get()
-      args = args.concat(['-p', `${hostPort}:3000`])
+      args = args.concat(['-p', `${hostPort}:${port}`])
       Logger.info(`FunctionWatcher will listen on ${hostPort}`)
     }
 
-    args.push(this.imageName)
+    args.push(imageName)
     Logger.info(`Run args`, { args })
     return args
   }
 }
-
-export { DockerContainer }
--- src/resources/DockerInterface/HostPortProvider.ts
+++ src/resources/DockerInterface/HostPortProvider.ts
@@ -1,10 +1,22 @@
 const INITIAL_PORT = 8888
-const TOTAL_PORTS = 5
+const TOTAL_PORTS = 200
+
+const shuffleArray = (array: any[]) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+}
 
 export class HostPortProvider {
   static ports: number[] = Array.from(Array(TOTAL_PORTS), (_, i: number) => i + INITIAL_PORT)
 
+  private static shuffle() {
+    shuffleArray(this.ports)
+  }
+
   public static get() {
+    HostPortProvider.shuffle()
     return HostPortProvider.ports.pop()
   }
 
