314f60f2cffcb30c035373a2849028f074244a1e
tiagonapoli <napoli.tiago96@gmail.com>
2019-07-05 14:59:41 -0300

    Create RedisWrapper

---
+++ src/resources/RedisWrapper.ts
@@ -0,0 +1,67 @@
+import { Logger } from './../utils/Logger'
+import redis from 'redis'
+import R from 'ramda'
+
+type Listener = (...args: any) => void
+interface ActiveSubscriptions {
+  [key: string]: { id: string; listener: Listener }[]
+}
+
+class NoSuchListener extends Error {
+  constructor(listener: string) {
+    super(`No such Listener ${listener}`)
+  }
+}
+
+export class RedisWrapper {
+  public static client = redis.createClient({
+    host: 'event-broker',
+    port: 6379,
+  })
+
+  private static subscriptions: ActiveSubscriptions = {}
+
+  public static addSubscription(channelName: string, listenerID: string, listener: Listener) {
+    const listenerWrapper = (channel: string, ...args: any) => {
+      Logger.info(`RedisEvent `, { channel, ...args })
+      if (channel === channelName) listener(channel, ...args)
+    }
+    this.client.subscribe(channelName)
+    this.client.addListener('message', listenerWrapper)
+    if (this.subscriptions[channelName] == null) {
+      this.subscriptions[channelName] = []
+    }
+
+    this.subscriptions[channelName].push({
+      id: listenerID,
+      listener: listenerWrapper,
+    })
+
+    Logger.info(`Added subscription to channel ${channelName}, ${listenerID}`)
+  }
+
+  public static removeSubscription(channelName: string, listenerID: string) {
+    const listenerIndex = R.findIndex(el => el.id === listenerID, this.subscriptions[channelName])
+
+    if (listenerIndex === -1) throw new NoSuchListener(listenerID)
+    const { listener } = this.subscriptions[channelName].splice(listenerIndex, 1)[0]
+    this.client.removeListener('message', listener)
+
+    if (this.subscriptions[channelName].length === 0) {
+      delete this.subscriptions[channelName]
+      this.client.unsubscribe(channelName)
+    }
+
+    Logger.info(`Removed subscription to channel ${channelName}, ${listenerID}`)
+  }
+}
+
+if (process.env.NODE_ENV === 'development') {
+  RedisWrapper.client.monitor((err, res) => {
+    Logger.info('Redis client entered monitor mode')
+  })
+
+  RedisWrapper.client.on('monitor', (time, args, raw_reply) => {
+    console.log(`[redis monitor] ${time}: ${args}`)
+  })
+}
