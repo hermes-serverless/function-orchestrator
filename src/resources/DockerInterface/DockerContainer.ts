9d426537963ea7bdc2850fb84b9c03e496d61001
tiagonapoli <tiago.napoli@vtex.com.br>
2019-08-11 17:41:24 -0300

    Fix DockerContainer and add tests

--- src/resources/DockerInterface/DockerContainer.ts
+++ src/resources/DockerInterface/DockerContainer.ts
@@ -2,7 +2,7 @@ import execa, { ExecaChildProcess } from 'execa'
 import { Logger } from '../../utils/Logger'
 import { HostPortProvider } from './HostPortProvider'
 
-interface DockerContainerOptions {
+export interface DockerContainerOptions {
   imageName: string
   gpuCapable: boolean
   detach: boolean
@@ -14,13 +14,7 @@ interface DockerContainerOptions {
 
 export class DockerContainer {
   dockerProc: ExecaChildProcess<string>
-  id: string
-  opts: DockerContainerOptions
-
-  constructor(id: string, options: DockerContainerOptions) {
-    this.opts = options
-    this.id = id
-  }
+  constructor(private id: string, private options: DockerContainerOptions) {}
 
   public start = () => {
     this.dockerProc = execa(process.env.DOCKER_BINARY_PATH || '/usr/bin/docker', this.createRunArgs())
@@ -29,22 +23,21 @@ export class DockerContainer {
   }
 
   private createRunArgs() {
-    const { imageName, gpuCapable, detach, port, network, envVariables, dnsName } = this.opts
+    const { imageName, gpuCapable, detach, port, network, envVariables, dnsName } = this.options
 
     let args = ['run']
     if (gpuCapable) args.push('--runtime=nvidia')
-    if (!port) args = args.concat([`-e`, `PORT=${port}`])
-    if (!dnsName) args.push(`--name=${dnsName}`)
-    if (!network) args.push(`--network=${network}`)
+    if (port) args = args.concat([`-e`, `PORT=${port}`])
+    if (dnsName) args.push(`--name=${dnsName}`)
+    if (network) args.push(`--network=${network}`)
     if (detach) args.push('-d')
-
-    if (!envVariables) {
+    if (envVariables) {
       envVariables.forEach(envVar => {
         args = args.concat(['-e', envVar])
       })
     }
 
-    if (process.env.NODE_ENV === 'development') {
+    if (process.env.NODE_ENV === 'development' && port) {
       const hostPort = HostPortProvider.get()
       args = args.concat(['-p', `${hostPort}:${port}`])
       Logger.info(`FunctionWatcher will listen on ${hostPort}`)
